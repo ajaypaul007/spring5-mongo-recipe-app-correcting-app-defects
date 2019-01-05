f8ace995003b915f6dc934d83a951962e8f6d96d
ajaypaul007 <ajaypaul007@gmail.com>
2019-01-05 09:45:40 -0500

    Initial commit

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,49 @@
+package guru.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+@Getter
+@Setter
+@Document
+public class Category {
+    @Id
+    private String id;
+    private String description;
+
+    @DBRef
+    private Set<Recipe> recipes;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
